513f9296e69ed6765bfb587801abcd8c1071c3df
RezaHayati <114706894+rezhnncoding@users.noreply.github.com>
2022-10-25 19:07:09 +0330

    main file

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"bookproject/routes"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStore(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:4040", r))
+}
